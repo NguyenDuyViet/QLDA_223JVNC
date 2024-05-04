153552059000c3fdfa065a3ada1e9d7e06763f7e
khanhnguvcl <nguyenvukhanh392@gmail.com>
2024-05-04 13:40:21 +0700

    Update SQL and add ERD

--- QLDOGIADUNG.sql
+++ QLDOGIADUNG.sql
@@ -4,7 +4,7 @@ use QLSHOPDOGIADUNG
 go
 create table Users
 (
-	MaKH char(7) primary key,
+	MaKH char(5) primary key,
 	Ho nvarchar(10),
 	Ten nvarchar(30),
 	SDT char(10) unique,
@@ -12,44 +12,58 @@ create table Users
 	DiaChi nvarchar(50)	,
 	MatKhau varchar(20)
 )
-create table DonDatHang
-(
-	MaDatHang char(7) primary key,
-	TrangThaiDatHang integer default '1' /* 0 là hủy đặt hàng, 1 là đặt thành công*/,
-	TrangThaiGiaoHang integer default '1' /* 0 là khách hủy đặt hàng, 1 là đang giao hàng, 2 là giao hàng thành công*/,
-	NgayDatHang date,
-	NgayGiaoHang date,
-	SoLuong integer check (SoLuong >=0),
-	MaKH char(7) foreign key references Users(MaKH)
-	ON DELETE CASCADE  ON UPDATE CASCADE
-)
-create table HangDuocDat
-(
-	MaHang char(7) primary key,
-	Gia money check (Gia >=0),
-	MaDatHang char(7) foreign key references DonDatHang(MaDatHang),
-	MaSanPham char(7) foreign key references SanPham(MaSanPham)
-	ON DELETE CASCADE  ON UPDATE CASCADE
-)
-create table SanPham
-(
-	MaSanPham char(7) primary key,
-	TenSanPham nvarchar(30),
-	GiaBan money check (GiaBan >=0),
-	MaLoai char(7) foreign key references KhoHang(MaLoai),
-	MaHang char(7) foreign key references Hang(MaHang)
-	ON DELETE CASCADE  ON UPDATE CASCADE
-)
 create table KhoHang
 (
-	MaLoai char(7) primary key,
+	MaLoai char(5) primary key,
 	TenLoai nvarchar(50),
 	SoLuongNhapVao integer,
 	GiaNhap money check (GiaNhap >=0),
 	TonKho integer	
 )
-create table Hang /*Hãng*/
+create table SanPham
 (
-	MaHang char(7) primary key,
-	TenHang nvarchar(20)
+	MaSanPham char(5) primary key,
+	TenSanPham nvarchar(30),
+	GiaBan money check (GiaBan >=0),
+	MaLoai char(5) foreign key references KhoHang(MaLoai)
+	--MaHang char(7) foreign key references Hang(MaHang)
+	ON DELETE CASCADE  ON UPDATE CASCADE
 )
+
+create table HoaDonDatHang
+(
+	MaHoaDon int identity(1,1) primary key,
+	--TrangThaiDatHang nvarchar(20) ,
+	TrangThaiGiaoHang nvarchar(20),
+	NgayDatHang date,
+	NgayGiaoHang date,
+	SoLuong integer check (SoLuong >=0),
+	GiaMua money check (GiaMua >= 0),
+	MaKH char(5) foreign key references Users(MaKH),
+	MaSanPham char(5) foreign key references SanPham(MaSanPham)
+	ON DELETE CASCADE  ON UPDATE CASCADE
+)
+
+create table GioHang
+(
+	MaGioHang char(5) primary key,
+	MaKH char(5) foreign key references Users(MaKH),
+	MaSanPham char(5) foreign key references SanPham(MaSanPham)
+	ON DELETE CASCADE  ON UPDATE CASCADE
+)
+
+--Vài ràng buộc
+alter table Users
+add constraint email_check check (
+    Email like '[a-z]%@gmail.com'
+    AND Email NOT LIKE '%@%@%'
+)
+alter table Users
+add constraint SDT_check check (SDT like '0[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]');
+alter table HoaDonDatHang
+add constraint trangthaiGiaoHang_check check (TrangThaiGiaoHang IN (N'Chờ xác nhận', N'Chờ lấy hàng', N'Đang giao', N'Đã giao', N'Đã hủy', N'Trả hàng', N'Đánh giá'));
+alter table HoaDonDatHang
+add constraint kiemtrangaygiaohang_check check (NgayGiaoHang >= NgayDatHang);
+
+--drop database QLSHOPDOGIADUNG
+
