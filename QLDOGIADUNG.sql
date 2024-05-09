b2c19fa588050d786b769286bdf8945d129a507d
khanhnguvcl <nguyenvukhanh392@gmail.com>
2024-05-09 19:51:31 +0700

    Update lai nha

--- QLDOGIADUNG.sql
+++ QLDOGIADUNG.sql
@@ -14,19 +14,17 @@ create table Users
 )
 create table KhoHang
 (
-	MaLoai char(5) primary key,
-	TenLoai nvarchar(50),
-	SoLuongNhapVao integer,
-	GiaNhap money check (GiaNhap >=0),
-	TonKho integer	
+	MaLoai varchar(5) primary key,
+	TenLoai nvarchar(50)
 )
+
 create table SanPham
 (
 	MaSanPham char(5) primary key,
 	TenSanPham nvarchar(30),
-	GiaBan money check (GiaBan >=0),
-	MaLoai char(5) foreign key references KhoHang(MaLoai)
-	--MaHang char(7) foreign key references Hang(MaHang)
+	GiaBan bigint ,
+	SoLuong integer,
+	MaLoai varchar(5) foreign key references KhoHang(MaLoai)
 	ON DELETE CASCADE  ON UPDATE CASCADE
 )
 
@@ -37,8 +35,8 @@ create table HoaDonDatHang
 	TrangThaiGiaoHang nvarchar(20),
 	NgayDatHang date,
 	NgayGiaoHang date,
-	SoLuong integer check (SoLuong >=0),
-	GiaMua money check (GiaMua >= 0),
+	SoLuong integer ,
+	GiaMua bigint ,
 	MaKH char(5) foreign key references Users(MaKH),
 	MaSanPham char(5) foreign key references SanPham(MaSanPham)
 	ON DELETE CASCADE  ON UPDATE CASCADE
@@ -65,5 +63,23 @@ add constraint trangthaiGiaoHang_check check (TrangThaiGiaoHang IN (N'Chờ xác
 alter table HoaDonDatHang
 add constraint kiemtrangaygiaohang_check check (NgayGiaoHang >= NgayDatHang);
 
---drop database QLSHOPDOGIADUNG
+drop database QLSHOPDOGIADUNG
 
+insert into KhoHang(MaLoai, TenLoai) values ('TL',N'Tủ Lạnh'),
+											('HB',N'Thiết bị hút bụi'),
+											('LVS',N'Lò vi sóng'),
+											('B',N'Bếp'),
+											('NC',N'Nồi chảo')
+insert into SanPham(MaSanPham,TenSanPham,GiaBan,SoLuong,MaLoai) values	('B0001',N'Bếp gas mini',200000,20,'B'), 
+																		('NC001',N'Bộ nồi chảo',500000,10,'NC'),
+																		('C0001',N'Chảo chống dính',70000,40,'NC'),
+																		('N0001',N'Nồi cơm điện Apricots',100000,60,'NC'),
+																		('VS001',N'Lò vi sóng',300000,20,'LVS'),
+																		('TL001',N'Tủ lạnh',1000000,30,'TL'),
+																		('HB001',N'Máy hút bụi',200000,70,'HB'),
+																		('HB002',N'Robot hút bụi',500000,70,'HB')
+insert into Users(MaKH,Ho,Ten,SDT,Email,DiaChi,MatKhau) 
+values	('K0001',N'Nguyễn',N'Văn A','0123456789','nva@gmail.com',N'48 Cao Thắng, Hải Châu, Đà Nẵng','nguyenvana'),
+		('K0002',N'Nguyễn',N'Văn B','0123456788','nvb@gmail.com',N'48 Cao Thắng, Hải Châu, Đà Nẵng','nguyenvanb'),
+		('K0003',N'Nguyễn',N'Văn C','0123456787','nvc@gmail.com',N'48 Cao Thắng, Hải Châu, Đà Nẵng','nguyenvanc'),
+		('K0004',N'Nguyễn',N'Văn D','0123456786','nvd@gmail.com',N'48 Cao Thắng, Hải Châu, Đà Nẵng','nguyenvand')
